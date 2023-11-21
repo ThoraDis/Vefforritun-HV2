4ce12a5f4f8721b8b54584990507c6cf064f0fc8
Patti103 <pej10@hi.is>
2023-11-21 14:55:24 +0000

    Tæki og tól bæt við

---
+++ README.md
@@ -0,0 +1,38 @@
+# Vefforritun-HV2
+
+## Um síðuna
+Þetta er síða um vefforritunarbúðina sem sýnir allar vörur sem þar eru til.
+
+## Um verkefnið
+Í þessu verkefni eru þrjár síður.
+
+HTML:
+index.html: Er forsíða verkefnsins.
+vorulisti.html: Er listi yfir öllum vörum í Vefforritunarbúðini.
+vorusida.html: Innihelddur upplýsingar um eina vöru.
+
+CSS:
+
+
+JavaScript:
+scripts.js: JavaScript fyrir forsíða og byrtit sex nýjustu vörurnar.
+vorulisti.js: JavaScript fyrir vörulista sem útbýr stuðning við leit.
+vorusida.js: JavaScript fyrir vörusíðu sem ...
+
+## Hvernig skal keyra verkefnið
+Verkefnið skal keyra með npm run dev.
+En til að keyra stlyelint skal skrifa npm run lint.
+
+
+## Hópurinn
+Þóra Dís Garðarsdóttir:
+
+GitHub: ThoraDis
+
+Netfang: tdg5@hi.is
+
+Patrekur:
+
+GitHub: Patti103
+
+Netfang: pej10@hi.is
---
+++ scripts.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?limit=6')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const productContainer = document.getElementsByClassName("boxes")[0];
+
+            console.log(data);
+
+            if (data && Array.isArray(data.items)) {
+                data.items.forEach(product => {
+                    
+                    const productDiv = document.createElement("div");
+                    productDiv.className = "box";
+
+                    
+                    const categoryName = product.category_title || 'N/A';
+
+                    
+                    productDiv.innerHTML = `
+                        <img src="${product.image}" alt="Product Image">
+                        <h3>${product.title}</h3>
+                        <p>${product.price} kr.-</p>
+                        <p>${categoryName}</p>
+                    `;
+
+                    
+                    productContainer.appendChild(productDiv);
+                });
+            } else {
+                console.error('Invalid data structure:', data);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+});

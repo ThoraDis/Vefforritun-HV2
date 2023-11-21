f83e75f10ec3ae5f377400f91d4b3e4567e519e8
Patti103 <pej10@hi.is>
2023-11-21 15:20:32 +0000

    ..

--- sidur/vorulisti.js
+++ sidur/vorulisti.js
@@ -1,42 +1,79 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            const productContainer = document.getElementsByClassName("boxes")[0];
+function performSearch() {
+    const searchInput = document.getElementById('input');
+    const query = searchInput.value.trim();
 
-            console.log(data);
+   
+    fetchData(query);
 
-            if (data && Array.isArray(data.items)) {
-                data.items.forEach(product => {
-                    
-                    const productDiv = document.createElement("div");
-                    productDiv.className = "box";
+   
+    history.pushState({ query }, '', `?search=${encodeURIComponent(query)}`);
+  }
 
-                    
-                    const categoryName = product.category_title || 'N/A';
 
-                    
-                    productDiv.innerHTML = `
-                        <img src="${product.image}" alt="Product Image">
-                        <h3>${product.title}</h3>
-                        <p>${product.price} kr.-</p>
-                        <p>${categoryName}</p>
-                    `;
+  function fetchData(searchQuery) { 
+    const productContainer = document.querySelector('.boxes');
 
-                    
-                    productContainer.appendChild(productDiv);
-                });
-            } else {
-                console.error('Invalid data structure:', data);
-            }
+   
+    fetch(`https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/products?search=${searchQuery}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
         
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+        productContainer.innerHTML = '';
+
+        if (data && Array.isArray(data.items) && data.items.length > 0) {
+          
+          data.items.forEach(product => {
+            const productDiv = document.createElement("div");
+            productDiv.className = "box";
+            productDiv.innerHTML = `
+              <img src="${product.image}" alt="Product Image">
+              <h3>${product.title}</h3>
+              <p>${product.price} kr.-</p>
+              <p>${product.category_title || 'N/A'}</p>
+            `;
+            productContainer.appendChild(productDiv);
+          });
+        } else {
+          
+          productContainer.innerHTML = '<p>Engar niðurstöður</p>';
+        }
+      })
+      .catch(error => {
+        console.error('Villa að ná í gögn:', error);
+      });
+  }
+
+  
+function restoreSearch() {
+const searchInput = document.getElementById('input');
+const query = getSearch();
+
+searchInput.value = query;
+fetchData(query);
+
+if (query) {
+    fetchData(query);
+} else {
+    
+    fetchData('');
+}
+}
+
+
+function getSearch() {
+const urlSearchParams = new URLSearchParams(window.location.search);
+return urlSearchParams.get('search') || '';
+}
+
+
+document.addEventListener('DOMContentLoaded', restoreSearch);
+
+
+window.addEventListener('popstate', function (event) {
+location.reload();
 });
